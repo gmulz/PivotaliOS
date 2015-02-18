de344f14a53104a1af57ff376cb39c6debea5ac9
gmulz <themulitzer@gmail.com>
2015-02-18 10:10:33 -0500

    Initial Commit

---
+++ PivotalTests/PivotalTests.swift
@@ -0,0 +1,36 @@
+//
+//  PivotalTests.swift
+//  PivotalTests
+//
+//  Created by G Mulitz on 2/18/15.
+//  Copyright (c) 2015 G Mulitz. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PivotalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
